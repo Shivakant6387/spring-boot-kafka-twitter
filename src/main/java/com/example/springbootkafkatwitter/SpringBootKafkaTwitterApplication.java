6847d48e81ab79f23f1e49fdf2e7d84500ddb2aa
Shivakant6387 <shivakant5875@gmail.com>
2023-06-03 22:07:10 +0530

    no thing to commit

---
+++ src/main/java/com/example/springbootkafkatwitter/SpringBootKafkaTwitterApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootkafkatwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaTwitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootkafkatwitter/SpringBootKafkaTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootkafkatwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaTwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
